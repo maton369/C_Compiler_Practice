faa6f3d534ab1fc92c815cb22f8e1f47df228b11
maton369 <ysk901085@gmail.com>
2025-06-16 12:02:39 +0900

    Add: テストスクリプトを作成

---
+++ 9cc/exec.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cc -o 9cc 9cc.c 
+# ./9cc 123 > tmp.s
+# cat tmp.s
+
+# cc -o tmp tmp.s -no-pie
+# ./tmp
+# echo $?
--- 9cc/exex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cc -o 9cc 9cc.c 
-./9cc 123 > tmp.s
-cat tmp.s
